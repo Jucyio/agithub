46111875d3712f4dce39b7deac6b0fc8583e7a33
Uriel Corfa <uriel@corfa.fr>
2015-03-24 20:42:11 -0700

    Add a setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='agithub',
+      version='1.0',
+      description='The agnostic Github API client',
+      author='Jucy.io',
+      author_email='hello@jucy.io',
+      url='https://github.com/jucyio/agithub/',
+      py_modules=['agithub'],
+     )
